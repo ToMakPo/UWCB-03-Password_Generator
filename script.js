cf559d6756f72756b07276b51be88f91ce0853af
Makai Post <postmakai@gmail.com>
2020-10-19 10:11:56 -0700

    changed wording of error messages.

--- script.js
+++ script.js
@@ -29,15 +29,15 @@ function generatePassword() {
     // Make sure the value given is a number and is 
     // between or equal to 8 and 128.
     if (pw_length == NaN) {
-      error_msg = "That wasn't a number, dumb ass. So I'll ask you again."
+      error_msg = "That wasn't a number. Please try again."
     } else if (pw_length_responce == "") {
-      error_msg = "Um? Did you forget to type something in? I'm not seeing any answer."
+      error_msg = "No answer was provided. Please try again."
     } else if (pw_length < 0) {
-      error_msg = "You arn't very smart, are you? That would be a negative number. Stop fucking aroung."
+      error_msg = "You can not pass in a negative number. Please try again."
     } else if (pw_length < 8) {
-      error_msg = "Oh. I guess you want want to making it easy for the bad guy. Your password needs to be at least 8 charaters long."
+      error_msg = "That was too short. The password must have a length of at least 8 characters."
     } else if (pw_length > 128) {
-      error_msg = "Fuck man! There is no way you are going to remember that. Let's try to keep is no more than 128 characters."
+      error_msg = "That was too long. The password must have a length of at most 8 characters."
     } else {
       // console.info(`User selected to have a password that is ${pw_length} characters long.`)
       break
@@ -55,7 +55,7 @@ function generatePassword() {
   while (true) {
     var pw_charaters = prompt((error_msg ? error_msg + "\n" : "") + msg)
 
-    var list = []
+    // var list = []
     var pw_lowercase = false
     var pw_uppercase = false
     var pw_numeric = false
@@ -68,41 +68,41 @@ function generatePassword() {
         case 'L': if (!pw_lowercase) {
           pw_lowercase = true
           pw_options += pwc_lowercase
-          list.push('lowercase')
+          // list.push('lowercase')
         }; break
         case 'U': if (!pw_uppercase) {
-          pw_lowercase = true
+          pw_uppercase = true
           pw_options += pwc_uppercase
-          list.push('uppercase')
+          // list.push('uppercase')
         }; break
         case 'N': if (!pw_numeric) {
-          pw_lowercase = true
+          pw_numeric = true
           pw_options += pwc_numeric
-          list.push('numeric')
+          // list.push('numeric')
         }; break
         case 'S': if (!pw_special) {
-          pw_lowercase = true
+          pw_special = true
           pw_options += pwc_special
-          list.push('special')
+          // list.push('special')
         }; break
       }
     }
 
     if (pw_options != "") {
-      let print = ""
-      switch (list.length) {
-        case 1: print = list[0]; break
-        case 2: print = `${list[0]} and ${list[1]}`; break
-        case 3: print = `${list[0]}, ${list[1]}, and ${list[2]}`; break
-        case 4: print = `${list[0]}, ${list[1]}, ${list[2]}, and ${list[3]}`; break
-      }
+      // let print = ""
+      // switch (list.length) {
+      //   case 1: print = list[0]; break
+      //   case 2: print = `${list[0]} and ${list[1]}`; break
+      //   case 3: print = `${list[0]}, ${list[1]}, and ${list[2]}`; break
+      //   case 4: print = `${list[0]}, ${list[1]}, ${list[2]}, and ${list[3]}`; break
+      // }
 
       // console.info(`The user decided to make the password contain ${print} characters.`)
       break
     } else if (pw_charaters == "") {
-      error_msg = "Dude! You didn't even type anything. Do you know how to follow simple instructions?"
+      error_msg = "No answer was provided. Please try again."
     } else {
-      error_msg = "What is wrong with you? That wasn't one of the options."
+      error_msg = "The answer provided was not one of the options. Please select at least one."
     }
   }
 
